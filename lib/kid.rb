4ca48f066cbedab52525d34bbcb0aec65be8146d
Erin Stansbury <ekstansbury@gmail.com>
2020-03-20 14:00:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,9 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-    include Dance
-    extend MetaDancing
-    
+    include Dance FancyDance::InstanceMethods
+    extend MetaDancing FancyDance::ClassMethods
+
     attr_accessor :name
 
     def initialize(name)
